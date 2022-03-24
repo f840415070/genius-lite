db831a688feee9a5cb1552f422e5641ea6ce28e4
fanyibin <f84041507@163.com>
2022-03-24 17:37:30 +0800

    feat: 测试爬虫

--- example/test.py
+++ example/test.py
@@ -1,3 +1,19 @@
-from genius_lite.utils.logger import Logger
+from genius_lite.core.genius_lite import GeniusLite
 
-Logger.instance('123')
+class TestSpider(GeniusLite):
+    spider_name = 'TestSpider'
+
+    def start(self):
+        yield self.request(
+            url='https://www.futbin.org/futbin/api/getFilteredPlayers',
+            parser=self.parse,
+            params={'page': 1, 'league': 19}
+        )
+
+    def parse(self, response):
+        self.logger.debug(response.json())
+
+
+if __name__ == '__main__':
+    spider = TestSpider()
+    spider.run()
