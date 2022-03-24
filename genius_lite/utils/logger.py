4a09c5f3c907aca9abc733f7c685a98aa5acd5c5
fanyibin <f84041507@163.com>
2022-03-24 17:38:28 +0800

    feat: 添加 log 输出颜色设置

--- genius_lite/utils/logger.py
+++ genius_lite/utils/logger.py
@@ -1,32 +1,45 @@
 import sys
 import os
+import colorlog
 import logging
 from logging.handlers import TimedRotatingFileHandler
+
+log_colors = {
+    'DEBUG': 'white',
+    'INFO': 'green',
+    'WARNING': 'yellow',
+    'ERROR': 'red',
+    'CRITICAL': 'bold_red',
+}
+
+log_format = '[%(levelname)s] %(asctime)s -> %(filename)s (line:%(lineno)d) -> %(name)s: %(message)s'
 
 class Logger:
     __instance = None
 
-    def __init__(self, name, **kwargs):
-        self.__enable = kwargs.get('log_enable') != False
-        level = kwargs.get('log_level') or 'INFO'
-        format = kwargs.get('log_format') or '%(asctime)s - %(levelname)s - %(name)s: %(message)s'
-        output = kwargs.get('log_output')
+    def __init__(self, name, **spider_config):
+        self.enable = spider_config.get('log_enable') != False
+        level = spider_config.get('log_level') or 'DEBUG'
+        format = spider_config.get('log_format') or log_format
+        output = spider_config.get('log_output')
 
-        self.__logger = logging.getLogger(name)
-        self.__logger.setLevel(level)
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level)
 
-        if self.__enable:
-            self.__logger.addHandler(self.stream_handler(level, format))
+        if self.enable:
+            self.logger.addHandler(self.stream_handler(level, format))
             if output:
                 self.check_output_path(output)
-                self.__logger.addHandler(self.file_handler(name, level, format, output))
+                self.logger.addHandler(self.file_handler(name, level, format, output))
 
-        self.__logger.propagate = False
+        self.logger.propagate = False
 
     def stream_handler(self, level, format):
         handler = logging.StreamHandler(sys.stderr)
         handler.setLevel(level)
-        handler.setFormatter(logging.Formatter(format))
+        handler.setFormatter(colorlog.ColoredFormatter(
+            fmt='%(log_color)s' + format, log_colors=log_colors
+        ))
         return handler
 
     def file_handler(self, name, level, format, output):
@@ -41,36 +54,43 @@ class Logger:
         if not os.path.exists(path):
             raise FileNotFoundError(f'Directory not found: {path}')
 
-    def debug(self, msg):
-        if self.__enable:
-            self.__logger.debug(msg)
+    @property
+    def debug(self):
+        return self.logger.debug if self.enable else self.notset
 
-    def info(self, msg):
-        if self.__enable:
-            self.__logger.info(msg)
+    @property
+    def info(self):
+        return self.logger.info if self.enable else self.notset
 
-    def warning(self, msg):
-        if self.__enable:
-            self.__logger.warning(msg)
+    @property
+    def warning(self):
+        return self.logger.warning if self.enable else self.notset
 
-    def error(self, msg):
-        if self.__enable:
-            self.__logger.error(msg)
+    @property
+    def error(self):
+        return self.logger.error if self.enable else self.notset
+
+    @property
+    def critical(self):
+        return self.logger.critical if self.enable else self.notset
+
+    def notset(self, msg, *args, **kwargs):
+        pass
 
     @classmethod
-    def instance(cls, name = None, **kwargs):
+    def instance(cls, name = None, **spider_config):
         if not cls.__instance:
-            cls.__instance = cls(name, **kwargs)
+            cls.__instance = cls(name, **spider_config)
         return cls.__instance
 
 
 if __name__ == '__main__':
-    config = {
-        # 'log_enable': False,
-        'log_level': 'DEBUG',
-    }
+    config = {}
     logger = Logger.instance('example', **config)
     logger.debug('it is a debug msg')
     logger.info('it is a info msg')
     logger.warning('it is a warning msg')
     logger.error('it is a error msg')
+    logger.critical({'error': 'it is a critical msg'})
+    logger2 = Logger.instance()
+    logger2.info(id(logger) == id(logger2))
