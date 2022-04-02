b556be901992885eabbc04b1ccf0f42147803bec
fanyibin <f84041507@163.com>
2022-04-02 16:06:28 +0800

    feat: 使用 SeedBasket 管理 Seed 的添加和使用

--- genius_lite/core/genius_lite.py
+++ genius_lite/core/genius_lite.py
@@ -1,10 +1,10 @@
 import traceback
-from time import time
 from abc import ABCMeta, abstractmethod
-from collections.abc import Iterable
-from genius_lite.seed.seed import Seed
-from genius_lite.seed.seed_list import SeedList
+from typing import Generator
+
 from genius_lite.http.request import HttpRequest
+from genius_lite.seed.seed import Seed
+from genius_lite.seed.seed_basket import SeedBasket
 from genius_lite.utils.logger import Logger
 
 
@@ -14,17 +14,18 @@ class GeniusLite(metaclass=ABCMeta):
 
     def __init__(self):
         if not self.spider_name.strip():
-            self.spider_name = f'spider{int(round(time() * 1000))}'
-        self.__seed_list = SeedList()
+            self.spider_name = self.__class__.__name__
+        self.__seed_basket = SeedBasket()
         self.logger = Logger.instance(self.spider_name, **self.spider_config)
         self.http_request = HttpRequest(**self.spider_config)
 
     @abstractmethod
-    def start(self):
+    def start_requests(self) -> Generator[Seed, None, None]:
         pass
 
-    def request(self, url, parser, method=None, data=None, params=None,
-                cookies=None, headers=None, payload=None, encoding=None, **kwargs):
+    def make_seed(self, url, parser, method = 'GET', data = None,
+                  params = None, headers = None, payload = None,
+                  encoding = None, cookies = None, **kwargs):
         _parser = parser
         if hasattr(parser, '__call__'):
             _parser = parser.__name__
@@ -32,29 +33,22 @@ class GeniusLite(metaclass=ABCMeta):
                            headers=headers, payload=payload, encoding=encoding))
         return Seed(url=url, parser=_parser, **kwargs)
 
-    def __push_seeds(self, seeds):
-        if not isinstance(seeds, Iterable):
-            return
-        for seed in seeds:
-            if isinstance(seed, Seed):
-                self.__seed_list.push(seed)
-
     def __run_once(self):
-        seed = self.__seed_list.pop()
-        if not isinstance(seed, Seed):
-            self.logger.warning(f'Invalid seed: {seed}')
+        seed = self.__seed_basket.seed()
+        if not seed:
             return
         resp = self.http_request.execute(seed)
         if not resp:
             return
         try:
             resp.payload = seed.payload
-            results = getattr(self, seed.parser)(resp)
-            self.__push_seeds(results)
+            seeds = getattr(self, seed.parser)(resp)
+            self.__seed_basket.put(seeds)
         except:
             self.logger.error(f'\n{traceback.format_exc()}')
 
     def run(self):
-        self.__push_seeds(self.start())
-        while self.__seed_list.length:
+        start_seeds = self.start_requests()
+        self.__seed_basket.put(start_seeds)
+        while self.__seed_basket.has_seeds:
             self.__run_once()
