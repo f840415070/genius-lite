4c3f344b28289ae0ee75c956314b40e3ebdf893f
fanyibin <f84041507@163.com>
2022-04-06 10:49:34 +0800

    feat: 版本设置 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="genius_lite",
-    version="0.1.0",
+    version="0.2.0",
     keywords=("Spider", "Web Crawler"),
     description="A Light Spider(Web Crawler) System in Python",
     long_description="A Light Spider(Web Crawler) System in Python",
