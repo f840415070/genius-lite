064ef5b471c943d45c8d58f035054094d4696746
fanyibin <f84041507@163.com>
2022-04-07 23:09:28 +0800

    pref: 优化请求超时代码

--- genius_lite/http/request.py
+++ genius_lite/http/request.py
@@ -3,24 +3,29 @@ from time import time as current
 from requests import Session, Request, exceptions
 from genius_lite.log.logger import Logger
 
+TIMEOUT_EXCEPTIONS = (
+    exceptions.Timeout,
+    exceptions.ConnectTimeout,
+    exceptions.ReadTimeout
+)
 
-def responded(send_func):
+
+def on_request(send_func):
     def handler(*args, **kwargs):
+        logger = Logger.instance()
+
+        request = args[1]
+        logger.info('%s %s' % (request.method, request.url))
+
         start_time = current()
-        resp = send_func(*args, **kwargs)
-        end_time = int((current() - start_time) * 1000)
-        info = f'Response[{resp.status_code}] {resp.reason}, {end_time}ms'
-        Logger.instance().info(info)
-        return resp
+        response = send_func(*args, **kwargs)
+        logger.info('Response[%s] %s, %sms' % (
+            response.status_code,
+            response.reason,
+            int((current() - start_time) * 1000)
+        ))
 
-    return handler
-
-
-def prepared(prepare_func):
-    def handler(*args):
-        request = prepare_func(*args)
-        Logger.instance().info('%s %s' % (request.method, request.url))
-        return request
+        return response
 
     return handler
 
@@ -29,29 +34,40 @@ class HttpRequest:
     def __init__(self):
         self.logger = Logger.instance()
         self.session = Session()
+        self.retry_limit_num = 3
 
-    @responded
+    @on_request
     def send(self, request, **kwargs):
         return self.session.send(request, **kwargs)
 
-    @prepared
     def prepare_request(self, seed):
         return self.session.prepare_request(Request(**seed.prepare_req_kwargs))
 
     def request(self, seed):
+        retry_num = 0
         prepared_request = self.prepare_request(seed)
-        return self.send(prepared_request, **seed.send_req_kwargs)
+        while retry_num <= self.retry_limit_num:
+            try:
+                response = self.send(prepared_request, **seed.send_req_kwargs)
+                return response
+            except TIMEOUT_EXCEPTIONS:
+                self.logger.warning(
+                    'Timeout (retry times: %s/%s) '
+                    'when requesting %s' % (retry_num, self.retry_limit_num, seed.url)
+                )
+                retry_num += 1
+                if retry_num > 3:
+                    self.logger.warning(
+                        'The maximum number of retries has been reached. '
+                        'Drop request %s' % seed.url
+                    )
+        return None
+
 
     def parse(self, seed):
-        retry_count = 3
-        while retry_count:
-            try:
-                response = self.request(seed)
-                return response
-            except (exceptions.Timeout, exceptions.ConnectTimeout, exceptions.ReadTimeout):
-                retry_count -= 1
-                self.logger.warning('Timeout')
-            except:
-                self.logger.error('\n%s' % traceback.format_exc())
-                break
-        return None
+        try:
+            response = self.request(seed)
+            return response
+        except:
+            self.logger.error('\n%s' % traceback.format_exc())
+            return None
