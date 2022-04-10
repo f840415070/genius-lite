97a01405900f56ada16cfa399cc0fa20d24d6b33
fanyibin <f84041507@163.com>
2022-04-10 22:06:51 +0800

    feat: test code

--- example/test.py
+++ example/test.py
@@ -4,22 +4,28 @@ from genius_lite.core.genius_lite import GeniusLite
 class TestSpider(GeniusLite):
 
     def start_requests(self):
-        for i in range(2):
-            yield self.crawl(
-                url='https://www.baidu.com',
-                parser=self.parse,
-                params={'foo': i},
-                timeout=3,
-            )
+        yield self.crawl(
+            url='https://www.baidu.com',
+            parser=self.parse,
+            params={'foo': 1, 'bar': 2},
+            timeout=3,
+        )
+        yield self.crawl(
+            url='https://www.baidu.com',
+            parser=self.parse,
+            params={'bar': 2, 'foo': 1},
+            timeout=3,
+        )
 
     def parse(self, response):
-        for i in range(3):
-            yield self.crawl(
-                url='https://www.baidu.com',
-                parser=self.parse2,
-                params={'foo': i},
-                timeout=3,
-            )
+        ...
+        # for i in range(3, 5):
+        #     yield self.crawl(
+        #         url='https://www.baidu.com',
+        #         parser=self.parse2,
+        #         params={'foo': i},
+        #         timeout=3,
+        #     )
 
     def parse2(self, response):
         # self.logger.debug(response.payload)
