e9f5674af49c8067b0e82fbe3528c12d0a072013
fanyibin <f84041507@163.com>
2022-04-10 23:32:13 +0800

    feat: 添加创建 Request 对象方法

--- genius_lite/seed/seed.py
+++ genius_lite/seed/seed.py
@@ -1,15 +1,11 @@
-import hashlib
-
-
-def md5(content):
-    return hashlib.md5(content.encode()).hexdigest()[8:24]
+from requests import Request
+from genius_lite.utils.tool import md5, obj_to_str, str_sort
 
 
 class Seed:
     def __init__(self, url=None, parser=None, method=None, data=None, params=None, headers=None, payload=None,
                  encoding=None, cookies=None, files=None, json=None, auth=None, hooks=None, timeout=None, verify=None,
                  stream=None, cert=None, allow_redirects=None, proxies=None):
-        self.id = self.md5(url)
         self.url = url
         self.parser = parser
         self.method = method
@@ -30,19 +26,54 @@ class Seed:
         self.allow_redirects = allow_redirects
         self.proxies = proxies
 
-    def md5(self, target):
-        return hashlib.md5(str(target).encode()).hexdigest()[8:24]
+        self.id = self.create_id()
+        self.time = None
+
+    def __str__(self):
+        result = 'id[%s], url[%s], method[%s], parser[%s]' % (
+            self.id,
+            self.url,
+            self.method,
+            self.parser
+        )
+        if self.params:
+            result += ', params[%s]' % obj_to_str(self.params)
+        if self.data:
+            result += ', data[%s]' % obj_to_str(self.data)
+        if self.payload:
+            result += ', payload[%s]' % obj_to_str(self.payload)
+
+        return 'seed<%s>' % result
+
+    def create_id(self):
+        data = self.url + self.method
+        if self.params:
+            data += obj_to_str(self.params)
+        if self.data:
+            data += obj_to_str(self.data)
+        return md5(str_sort(data))
+
+    def create_request(self):
+        return Request(
+            url=self.url,
+            method=self.method,
+            data=self.data,
+            params=self.params,
+            headers=self.headers,
+            cookies=self.cookies,
+            files=self.files,
+            json=self.json,
+            auth=self.auth,
+            hooks=self.hooks
+        )
 
     @property
-    def prepare_req_kwargs(self):
-        return {
-            'url': self.url, 'method': self.method, 'data': self.data, 'params': self.params, 'headers': self.headers,
-            'cookies': self.cookies, 'files': self.files, 'json': self.json, 'auth': self.auth, 'hooks': self.hooks
-        }
-
-    @property
-    def send_req_kwargs(self):
-        return {
-            'timeout': self.timeout, 'verify': self.verify, 'stream': self.stream,
-            'cert': self.cert, 'allow_redirects': self.allow_redirects, 'proxies': self.proxies
-        }
+    def send_setting(self):
+        return dict(
+            timeout=self.timeout,
+            verify=self.verify,
+            stream=self.stream,
+            cert=self.cert,
+            allow_redirects=self.allow_redirects,
+            proxies=self.proxies
+        )
