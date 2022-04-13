6ecb66331c3e3ddd7bdc9ae689772340e50ee775
fanyibin <f84041507@163.com>
2022-04-13 17:03:58 +0800

    chore: 更新版本至 v0.2.4

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="genius_lite",
-    version="0.2.3",
-    keywords=("Spider", "Web Crawler"),
+    version="0.2.4",
+    keywords=("Spider", "Web Crawler", "requests"),
     description="A Light Spider(Web Crawler) System in Python",
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
